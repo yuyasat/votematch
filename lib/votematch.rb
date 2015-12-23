5f9d830ee9c53d31fc658bb27ac35420103e69a1
yuyataki <takiyour@gmail.com>
2015-12-23 22:34:13 +0900

    second commit

--- lib/votematch.rb
+++ lib/votematch.rb
@@ -1,5 +1,10 @@
 require "votematch/version"
-require "votematch/vote"
+require "votematch/model/party_info"
 
 module Votematch
+  class Votematch
+    def your_party
+      party_info.parties.sample
+    end
+  end
 end
---
+++ lib/votematch/model/party_info.rb
@@ -0,0 +1,6 @@
+module Votematch
+  class Party_info
+    attr_reader patries
+    parties = %w(自民党 民主党 維新の党 共産党)
+  end
+end
